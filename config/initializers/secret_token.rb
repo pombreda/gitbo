6757f47d62f10bbbce8a72b004d8f8380813509c
Josh Rowley <joshua.f.rowley@gmail.com>
2012-11-06 13:58:43 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitbo::Application.config.secret_token = 'b38ea1df83a6c34465a786efe8a6523a4073f4bc484fb574bedc9137cf61b76a0ad878143d34d120622337fd0e7cab9b23c06ce9e17dd94d9ddeea9992f67523'
