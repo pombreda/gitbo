a11eac4bca8f8af44c32b6ec456248bb2fdeb99b
Kevin <mcnameekm@gmail.com>
2012-11-11 14:12:11 -0500

    add gemfiles and worker file

--- app/workers/github_worker.rb
+++ app/workers/github_worker.rb
@@ -1,8 +1,10 @@
 class GithubWorker
   include Sidekiq::Worker
+
   sidekiq_options retry: false
 
   def perform(owner, repo, issue)
      Issue.create_from_github(owner, repo, issue) 
   end
+
 end
