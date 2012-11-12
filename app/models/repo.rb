9a7e2f244c237adfbb03b01f098e8b95ca24f9f6
Josh Rowley <joshua.f.rowley@gmail.com>
2012-11-12 16:51:08 -0500

    add slugs for repo names

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,4 +1,6 @@
 class Repo < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :name, :use => :slugged
   attr_accessible :description, :name, :open_issues, :owner_name, :watchers, :issues
  
   has_many :issues, :dependent => :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121112155534) do
+ActiveRecord::Schema.define(:version => 20121112214729) do
 
   create_table "issues", :force => true do |t|
     t.string   "title"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(:version => 20121112155534) do
     t.integer  "open_issues"
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
+    t.string   "slug"
   end
+
+  add_index "repos", ["slug"], :name => "index_repos_on_slug"
 
 end
