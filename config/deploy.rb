1a5cb7e028da78b171f4529556aeea0634b9842f
Josh Rowley <joshua.f.rowley@gmail.com>
2012-11-14 17:06:30 -0500

    test symlinking db from deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,4 +28,8 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
 end
