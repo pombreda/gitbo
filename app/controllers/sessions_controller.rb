208419c35718b95bc44992c154e7dd11b8f7da61
Josh Rowley <joshua.f.rowley@gmail.com>
2012-11-15 15:53:20 -0500

    remove extra debugger from sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    debugger
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     session[:token] = auth.credentials.token
