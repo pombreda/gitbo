606d163880851fb2a16ebab83efa521600e93241
Josh Rowley <joshua.f.rowley@gmail.com>
2012-11-16 15:26:43 -0500

    post rebase

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121116164536) do
+ActiveRecord::Schema.define(:version => 20121116165916) do
 
   create_table "bounties", :force => true do |t|
     t.integer  "user_id"
@@ -66,10 +66,10 @@ ActiveRecord::Schema.define(:version => 20121116164536) do
   create_table "user_votes", :force => true do |t|
     t.integer  "user_id"
     t.integer  "issue_id"
-    t.integer  "upvote"
-    t.integer  "downvote"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.integer  "upvote",     :default => 0
+    t.integer  "downvote",   :default => 0
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
   create_table "users", :force => true do |t|
