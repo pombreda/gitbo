bebda6a4218be1fd7e4d037a6a708e93ab1ea3bd
Kevin <mcnameekm@gmail.com>
2012-11-20 18:08:18 -0500

    conditional requests continued

--- app/workers/refresh_issues_worker.rb
+++ app/workers/refresh_issues_worker.rb
@@ -7,11 +7,11 @@ class RefreshIssuesWorker
     issue = Issue.find(174)
     repo = issue.repo
 
-    if my_client = Octokit::Client.new(:oauth_token => "b423c6264a4286b2ac12961792b02a33994c7dd6")
-      my_client.issue("#{repo.owner_name}/#{repo.name}", issue.git_number, :since => my_client.last_modified)
-        github_connection = GithubConnection.new(repo.owner_name, repo.name, issue.git_number)
-        issue.update_issue_attributes(github_connection)
-      end
+    my_client = Octokit::Client.new(:oauth_token => "b423c6264a4286b2ac12961792b02a33994c7dd6")
+    if my_client.issue("#{repo.owner_name}/#{repo.name}", issue.git_number, :since => my_client.last_modified)
+      github_connection = GithubConnection.new(repo.owner_name, repo.name, issue.git_number)
+      issue.update_issue_attributes(github_connection)
+    end
   end
 
-end
+endmy
