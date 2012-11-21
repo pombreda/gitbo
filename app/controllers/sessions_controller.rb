79a01d3cc895d6ef199551ba79bff98c523f7a8d
Adam Jonas <francestown81@gmail.com>
2012-11-21 12:13:53 -0500

    get initial Rails caching of user's following and starred

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,17 @@ class SessionsController < ApplicationController
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     session[:token] = auth.credentials.token
+
+    client = Octokit::Client.new(:oauth_token => auth.credentials.token)
+
+    Rails.cache.fetch(user.nickname.to_sym, expires_in: 24.hours) do
+      { :repos => client.repositories(user.nickname).collect {|repo| repo.name },
+        :following => client.following(user.nickname).collect {|user| user.login },
+        :starred => client.starred(user.nickname).collect {|repo| "#{repo.owner.login}/#{repo.name}" }  }
+    end
+
+
+
     redirect_to root_url, :notice => "Signed in!"
 
   end
