eff9cf1fc859b86ee2265419f1d6cdff36d726f3
Adam Jonas <francestown81@gmail.com>
2012-11-26 18:25:21 -0500

    avi refactored super long route new_issue_bounty_path

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,2 +1,6 @@
 module IssuesHelper
+
+  def link_to_new_bounty_for(issue, opts = {})
+    link_to "Issue Bounty", new_issue_bounty_path(issue.repo.owner_name, issue.repo.name, issue.git_number), :class => "btn btn-success btn-small", id: "new_link", remote: true
+  end
 end
