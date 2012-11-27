3040f4413a2df63db017a2ff853a55a26ff08624
Josh Rowley <joshua.f.rowley@gmail.com>
2012-11-27 14:53:23 -0500

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -35,16 +35,16 @@ ActiveRecord::Schema.define(:version => 20121127171931) do
   create_table "issues", :force => true do |t|
     t.string   "title"
     t.integer  "git_number"
-    t.text     "body",              :limit => 255
-    t.datetime "created_at",                                      :null => false
-    t.datetime "updated_at",                                      :null => false
+    t.text     "body"
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
     t.integer  "repo_id"
     t.integer  "comment_count"
     t.integer  "upvote"
     t.integer  "downvote"
     t.datetime "git_updated_at"
     t.string   "state"
-    t.integer  "owner_endorsement",                :default => 0
+    t.integer  "owner_endorsement", :default => 0
     t.string   "owner_name"
     t.string   "owner_image"
   end
@@ -52,11 +52,11 @@ ActiveRecord::Schema.define(:version => 20121127171931) do
   create_table "repos", :force => true do |t|
     t.string   "name"
     t.string   "owner_name"
-    t.text     "description",    :limit => 255
+    t.text     "description"
     t.integer  "watchers"
     t.integer  "open_issues"
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
     t.string   "slug"
     t.datetime "git_updated_at"
   end
