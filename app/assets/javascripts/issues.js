02e575067dca99228f67659348b691d618aa8159
Adam Jonas <francestown81@gmail.com>
2012-11-28 14:22:29 -0500

    the modal works and it adds the div

--- app/assets/javascripts/issues.js
+++ app/assets/javascripts/issues.js
@@ -22,3 +22,31 @@ $('div#refresh').click(function(){
     $('div#endorse').removeAttr('style');
     }
 
+//grab the numeric values of two fields
+//compare and make sure they are the same
+//submit form or return error
+
+  // var first=$('#bounty_price').val(); 
+  // var second=$('#bounty').val();
+
+  var first=$('button#pledge').live(
+    "click", (function() {
+    // alert('this was clicked');
+    $('#bounty_price_page').val();
+    // console.log(this);
+  }));
+  
+  // console.log(second;)
+  console.log(first);
+
+//   var first=document.getElementById('first').value;
+// var second=document.getElementById('second').value; 
+ 
+// if(parseInt(first)&lt;parseInt(second))
+// alert('Second Value is Greater than first');
+ 
+// else if(parseInt(first)&gt;parseInt(second))
+// alert('First Value is Greater than second');
+ 
+// else if(parseInt(first)==parseInt(second))
+// alert('First Value and second value is equal');
