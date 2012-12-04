19cdfffaa25eea2ba230f31741c6ea70abc17fff
Josh Rowley <joshua.f.rowley@gmail.com>
2012-12-04 15:44:14 -0500

    add one more column into the table sort jquery stuff since difficulty was added as a column

--- app/assets/javascripts/issues_table.js
+++ app/assets/javascripts/issues_table.js
@@ -52,7 +52,7 @@ jQuery(function() {
     "aoColumns": [
       null, null, null, null, {
         "sType": "num-html"
-      }
+      }, null
     ],
     "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
     sPaginationType: "bootstrap",
