b54c4521e246d4c514ce32b3a100a4185bc278c9
Josh Rowley <joshua.f.rowley@gmail.com>
2012-12-13 12:37:54 -0500

    write User.create_with_omniauth test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,42 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe ".create_with_omniauth" do
+
+    let (:auth) do
+      OmniAuth::AuthHash.new({  :provider => 'github',
+                                :uid => '12345',
+                                :info => {  :email => "foo@bar.com",
+                                            :image => "http://someimage.com/url",
+                                            :nickname => "foobar",
+                                            :name => "John Doe" }
+                            })
+    end
+
+    let (:user) { User.find_by_uid("12345") }
+
+    it "should save a user from the OmniAuth hash with the correct name" do  
+      User.create_with_omniauth(auth)
+      user.name.should == "John Doe"
+    end
+
+    it "should save a user from the OmniAuth hash with the correct email" do
+      User.create_with_omniauth(auth)
+      user.email.should == "foo@bar.com"
+    end
+
+    it "should save a user from the OmniAuth hash with the correct image url" do
+      User.create_with_omniauth(auth)
+      user.image.should == "http://someimage.com/url"
+    end
+
+    it "should save a user from the OmniAuth hash with the correct nickname" do
+      User.create_with_omniauth(auth)
+      user.nickname.should == "foobar"
+    end
+
+  end
+
+
 end
