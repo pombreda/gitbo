51ffe9f63c1b35f8dd95247c1c1321031e22bfed
Adam Jonas <francestown81@gmail.com>
2012-12-28 21:30:36 -0500

    fixed permissions to only read and write to public repos

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, CONFIG[:github_client_id], CONFIG[:github_client_secret],
-  scope: "user, repo, issues"
+  scope: "user, public_repo"
 end
